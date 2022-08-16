d704332a3d1bdfb3d06edb27f4fdd7a40be9a0d5
cankemaloglu <acankemaloglu@gmail.com>
2022-08-16 16:14:38 +0300

    some design changes

--- main.py
+++ main.py
@@ -29,26 +29,54 @@ lottie_download = load_lottieurl(lottie_url_download)
 lottie_dataset = load_lottieurl(lottie_url_dataset)
 
 col1, col2= st.columns(2)
-
 with col1:
     st_lottie(lottie_hello, key="hello")
 
 with col2:
     st.title('Welcome to Machine Learning Web App')
 
-
 st.write("""
 # Explore different classifier and datasets
 Which one is the best?
 """)
+
+# Sidebar Selectboxws
 
 dataset_name = st.sidebar.selectbox(
     "Select Dataset",
     ("Iris", "Breast Cancer", "Wine")
 )
 
-_left,_right = st.columns(2)
+classifier_name = st.sidebar.selectbox(
+    "Select Classifier",
+    ("KNN", "SVM", "Random Forest")
+)
+
+## Sidebar Classifier Info Link
+
+if classifier_name == "KNN":
+
+    classifier_info  = st. sidebar.info(f"Go to Scikit Learn [{classifier_name}](https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestNeighbors.html#sklearn.neighbors.NearestNeighbors) Page")
+elif classifier_name == "SVM":
+    classifier_info = st.sidebar.info(
+        f"Go to Scikit Learn [{classifier_name}](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html?highlight=svm) Page")
+else:
+    classifier_info = st.sidebar.info(
+        f"Go to Scikit Learn [{classifier_name}](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier) Page")
+
+
+# Show the selected ones with check mark
+
+_left, _right = st.columns(2)
 with _left:
-    st.write(f"## {dataset_name} Dataset")
+    st.write(f"## Dataset: {dataset_name} ")
 with _right:
-    st_lottie(lottie_dataset, key="dataset",loop=False ,speed=0.5, height=80,width=100)
+    st_lottie(lottie_dataset, key="dataset", loop=False, speed=0.5, height=70, width=100)
+
+_left1, _right2 = st.columns(2)
+with _left1:
+    st.write(f"## Classifier: {classifier_name} ")
+with _right2:
+    st_lottie(lottie_dataset, key="classifier", loop=False, speed=0.5, height=70, width=100)
+
+
