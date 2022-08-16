67289bb7bc98063c18fd0e78c68cbbad16e3870d
cankemaloglu <acankemaloglu@gmail.com>
2022-08-16 17:16:24 +0300

    last v

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://assets1.lottiefiles.com/packages/lf20_w51pcehl.json"
 lottie_url_download = "https://assets4.lottiefiles.com/private_files/lf30_t26law.json"
 lottie_url_dataset = "https://assets7.lottiefiles.com/packages/lf20_a4oisbpo.json"
+lottie_url_rocket = "https://assets2.lottiefiles.com/packages/lf20_l3qxn9jy.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 lottie_download = load_lottieurl(lottie_url_download)
 lottie_dataset = load_lottieurl(lottie_url_dataset)
+lottie_rocket = load_lottieurl(lottie_url_rocket)
 
 col1, col2= st.columns(2)
 with col1:
@@ -78,5 +80,115 @@ with _left1:
     st.write(f"## Classifier: {classifier_name} ")
 with _right2:
     st_lottie(lottie_dataset, key="classifier", loop=False, speed=0.5, height=70, width=100)
+
+#Getting data ready
+
+def get_dataset(name):
+    data = None
+    if name == "Iris":
+        data = datasets.load_iris()
+    elif name == "Wine":
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X,y
+
+st_lottie(lottie_rocket, key="rocket", speed=0.8, height=150, width=600)
+
+X,y = get_dataset(dataset_name)
+shape_col, class_col = st.columns(2)
+with shape_col:
+    st.write("### Shape of dataset", X.shape, )
+with class_col:
+    st.write("### Number of classes:", len(np.unique(y)))
+
+#Getting parameters & classifiers ready
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
+
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'],
+            max_depth=params['max_depth'], random_state=1234)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+
+#### CLASSIFICATION ####
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+st.write(f'### Accuracy =', acc)
+
+#### PLOT DATASET ####
+# Project the data onto the 2 primary principal components
+pca = PCA(2)
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig = plt.figure()
+plt.scatter(x1, x2,
+        c=y, alpha=0.3,
+        cmap='viridis')
+
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+
+#plt.show()
+st.pyplot(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
